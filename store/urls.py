83b60585f2352b13f58e614cac4627197b6286c5
JeromeMberia <jeromemberia@gmail.com>
2020-06-18 11:10:42 +0300

    Fix Buug:             -updating the post request for api specifying the field             -Adding more tables

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/product/', views.ProductBunchList.as_view()),
+    path('api/product/', views.ProductBatchList.as_view()),
 ]
